edd806147552c2c427af110ec17b696061115eb9
mbyn123 <1832376466@qq.com>
2022-09-24 18:37:16 +0800

    搭建项目基础架构

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+const {
+    merge
+} = require('webpack-merge');
+const baseConfig = require('./webpack.config')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin'); // 错误提示
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = merge(baseConfig,{
+    mode:'development',
+    entry: './examples/index.tsx',
+    devtool: "eval-cheap-module-source-map",
+    devServer: {
+        host:'127.0.0.1',
+        hot: true,
+        static: true,
+        port: '9527',
+        open: true,
+        compress: true, // 开启gzip压缩 针对开发模式下的文件
+        historyApiFallback: true, // 防止 history 路由刷新后空白
+    },
+    plugins:[
+        new FriendlyErrorsWebpackPlugin(),
+        new ReactRefreshWebpackPlugin(), // 添加热更新插件
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // 设置html模板
+            title: 'zan-react-ui', // 设置页面标题
+            inject: true, // 自动注入静态资源
+        }),
+    ]
+})
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+
+module.exports = {
+    entry: './lib/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        filename: '[name].[contenthash].js', // 打包文件名称
+        library: 'Gu',
+        libraryTarget: 'umd'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        alias:{
+            styles: path.resolve(__dirname, 'styles'),
+            examples: path.resolve(__dirname, 'examples'),
+            lib: path.resolve(__dirname, 'lib'),
+        },
+        modules: [path.resolve(__dirname, 'utils'), 'node_modules']
+    },
+    module: {
+        rules: [
+            {
+                test:/\.(tsx|jsx)$/,
+                exclude: /node-modules/,
+                use:[
+                    {
+                        loader: "babel-loader",
+                        options: {plugins: devMode?[require.resolve('react-refresh/babel')]:[]}
+                    }
+                ]
+            },{
+                test: /\.css$/,
+                use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+            },{
+                test: /\.s[ac]ss$/i,
+                use:[
+                    {
+                        loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            },{
+                test: /\.(svg|png|jpg|gif)$/,
+                type: "asset/resource",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024, // 小于10kb转base64位
+                    }
+                },
+                generator: {
+                    filename: 'static/images/[name][ext]', // 文件输出目录和命名
+                }
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+
+    ]
+}
